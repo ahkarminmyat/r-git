7daf6508446217c82e0511d8ce8464011c03b366
ahkarminmyat <dev.ahkarminmyat98@gmail.com>
2024-04-12 09:20:48 +0700

    check_db_size

---
+++ check_db.sql
@@ -0,0 +1,11 @@
+# check database size
+SELECT A.DatabaseName,SUM(A.Size) Size,SUM(A.SizeGB) SizeGB,SUM(A.SizeMB) SizeMB FROM
+(SELECT DB_NAME(database_id) AS DatabaseName,
+       Name AS Logical_Name,
+       Physical_Name,
+	   size Size,
+	   (size * 8) / 1024 SizeMB,
+       ((size * 8) / 1024) / 1024 SizeGB
+FROM sys.master_files
+WHERE database_id > 4) A
+GROUP BY A.DatabaseName
